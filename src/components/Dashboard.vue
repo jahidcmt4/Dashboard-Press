af3f0ec90e9bbd98945bcbc2d9f3553367279e26
jahid-dev <jahidhasancmt7@gmail.com>
2023-12-01 22:06:20 +0600

    dashboard and report design

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -3,7 +3,78 @@
 </script>
 
 <template>
-  <h2>This is Dashboard Page</h2>
+  <div class="deashboard-header">
+    <div class="flex flex-row bg-blue-950 rounded-xl pl-24 pr-24 pt-8 pb-8 gap-10">
+      <div class="basic-3/4">
+        <img class="w-48" src="http://blogfi.local/wp-content/plugins/tourfic/assets/app/images/tourfic-logo-white.png" alt="">
+        <h1 class="!text-4xl !font-extralight text-white">
+          Build & Manage Your Next
+          <b class="!font-bold">Travel or Hotel Booking Website</b>
+          with Tourfic
+        </h1>
+      </div>
+      <div class="basic-1/4 w-full">
+        <img src="http://blogfi.local/wp-content/plugins/tourfic/assets/app/images/hotel-booking-management-system@2x.webp" alt="">
+      </div>
+    </div>
+  </div>
+
+  <div class="report-box mt-10">
+    <h2 class="!font-bold !text-2xl mb-2">Overview</h2>
+    <div class="grid grid-cols-5 gap-4">
+
+      <div class="bg-white p-3 rounded-md flex flex-row gap-2.5">
+        <div class="details-icon w-10">
+          <img class="w-full" src="http://blogfi.local/wp-content/plugins/tourfic/assets/app/images/tf-hotel.png" alt="">
+        </div>
+        <div class="details-box">
+          <h5 class="!text-sm !font-semibold">Total Hotels</h5>
+          <h2 class="!text-2xl !font-bold">5</h2>
+        </div>
+      </div>
+
+      <div class="bg-white p-3 rounded-md flex flex-row gap-2.5">
+        <div class="details-icon w-10">
+          <img class="w-full" src="http://blogfi.local/wp-content/plugins/tourfic/assets/app/images/tf-tours.png" alt="">
+        </div>
+        <div class="details-box">
+          <h5 class="!text-sm !font-semibold">Total Tours</h5>
+          <h2 class="!text-2xl !font-bold">10</h2>
+        </div>
+      </div>
+
+      <div class="bg-white p-3 rounded-md flex flex-row gap-2.5">
+        <div class="details-icon w-10">
+          <img class="w-full" src="http://blogfi.local/wp-content/plugins/tourfic/assets/app/images/tf-apartment.png" alt="">
+        </div>
+        <div class="details-box">
+          <h5 class="!text-sm !font-semibold">Total Apartments</h5>
+          <h2 class="!text-2xl !font-bold">15</h2>
+        </div>
+      </div>
+
+      <div class="bg-white p-3 rounded-md flex flex-row gap-2.5">
+        <div class="details-icon w-10">
+          <img class="w-full" src="http://blogfi.local/wp-content/plugins/tourfic/assets/app/images/tf-booking-online.png" alt="">
+        </div>
+        <div class="details-box">
+          <h5 class="!text-sm !font-semibold">Total Bookings</h5>
+          <h2 class="!text-2xl !font-bold">15</h2>
+        </div>
+      </div>
+
+      <div class="bg-white p-3 rounded-md flex flex-row gap-2.5">
+        <div class="details-icon w-10">
+          <img class="w-full" src="http://blogfi.local/wp-content/plugins/tourfic/assets/app/images/tf-add-user.png" alt="">
+        </div>
+        <div class="details-box">
+          <h5 class="!text-sm !font-semibold">Total Customers</h5>
+          <h2 class="!text-2xl !font-bold">4</h2>
+        </div>
+      </div>
+
+    </div>
+  </div>
 </template>
 
 <style scoped>
--- src/components/Report.vue
+++ src/components/Report.vue
@@ -1,9 +1,35 @@
-<script setup>
+<script>
+import { Bar } from 'vue-chartjs'
+import { Chart as ChartJS, Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale } from 'chart.js'
 
+ChartJS.register(Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale)
+
+export default {
+  name: 'BarChart',
+  components: { Bar },
+  data() {
+    return {
+      chartData: {
+        labels: [ 'January', 'February', 'March' ],
+        datasets: [ { data: [40, 20, 12] } ]
+      },
+      chartOptions: {
+        responsive: true
+      }
+    }
+  }
+}
 </script>
 
 <template>
-<h2>Report Page</h2>
+    <div class="report-chart-box">
+        <h2 class="!font-bold !text-3xl mb-2">Reports</h2>
+        <Bar
+            id="my-chart-id"
+            :options="chartOptions"
+            :data="chartData"
+        />
+    </div>    
 </template>
 
 <style>
