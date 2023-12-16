f348865fcf8b4f59b4b6441f23d9d71c9c5a887a
jahid-dev <jahidhasancmt7@gmail.com>
2023-12-16 12:31:23 +0600

    instart added

--- src/components/Forms.vue
+++ src/components/Forms.vue
@@ -10,23 +10,22 @@ const news = ref('')
 
 const isOpen = ref(false)
 const isClick = ref(false)
+const NewsLoader = ref(true)
 
 // Retrive News
-onBeforeMount(() => {
-  axios.get(dashboardpressExtra.admin_url+'/wp-json/dashboardpress/v1/news')
-  .then(function (response) {
-    // handle success
+async function fetchNews() {
+  try {
+    const response = await axios.get(dashboardpressExtra.admin_url+'/wp-json/dashboardpress/v1/news');
     news.value = response.data.news;
-  })
-  .catch(function (error) {
-    // handle error
-    console.log(error);
-  })
-  .finally(function () {
-    // always executed
-  });
+    NewsLoader.value = false;
+  } catch (error) {
+    console.error(error);
+  }
+}
 
-})
+onBeforeMount(() => {
+  fetchNews();
+});
 
 // Add New News
 function formSubmit() {
@@ -42,6 +41,7 @@ async function createPost() {
   if(response.data.success){
     isOpen.value = false;
     isClick.value = false;
+    await fetchNews();
   }
 }
 
@@ -64,7 +64,7 @@ async function createPost() {
         </div>
         
         <div class="relative rounded-xl overflow-auto">
-            <div class="shadow-sm overflow-hidden my-8">
+            <div class="shadow-sm overflow-hidden my-8 relative">
             <table class="border-collapse table-auto w-full text-sm">
               <thead>
                 <tr>
@@ -83,6 +83,13 @@ async function createPost() {
                 </tr>
               </tbody>
             </table>
+
+            <div class="preloader-icon absolute h-full w-full left-0 top-0 flex items-center justify-center bg-white" v-show="NewsLoader">
+              <svg class="animate-spin -ml-1 mr-3 h-7 w-7 text-indigo-500" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+                <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
+                <path class="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
+              </svg>
+            </div>
           </div>
         </div>
     </div>
@@ -132,5 +139,6 @@ async function createPost() {
 </template>
 
 <style>
-/* Your CSS styles here */
+
+
 </style>
